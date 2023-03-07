943889f242badc3c789e5085d3a205502ab0ac0b
prithvi.yadav <prithvi.yadav.ug21@nsut.ac.in>
2023-03-07 12:17:51 +0530

    new commits

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 var preload = document.getElementById("loader");
 setTimeout(function load() {
-  preload.style.display = "none";
-}, 2000);
+  preload.style.opacity = "0";
+  preload.style.transform = "translate3d(-1520px, 0, 0)";
+},2000);
 
 var audioElement = false;
 $(".sound-div a").click(function () {
@@ -56,3 +58,29 @@ var tagCloud = TagCloud(".sphereContainer", Texts, {
 });
 
 document.querySelector(".sphereContainer").style.color = "#08fdd8";
+
+// don't forget to register it!
+// gsap.registerPlugin(DrawSVGPlugin);
+
+// show the SVG - hidden by default in CSS to prevent FOUC
+// gsap.set("#svg", {
+//   opacity: 1
+// });
+
+// // and then draw in your stroke!
+// gsap.from(".svg-logo", {
+//   duration: 3,
+//   ease: "sine.out",
+//   drawSVG: 0,
+//   repeat: -1,
+//   repeatDelay: 2
+// });
+// gsap.from(".svg-logo", {duration:1,});
+gsap.to(".animated-img", { opacity: 1, delay: 2.2,duration:3 });
+gsap.to(".svg-logo", { opacity: 0, delay: 2.2, duration: 15 });
+
+// for $i from 1 through 50({
+//    span img:nth-child(#{$i}n) {
+//       animation-delay: #{$i /10}s;
+//   }
+// });
