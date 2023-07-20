e1984154f181a5727d46e4122eaba975d1cf492f
prithvi.yadav <prithvi.yadav.ug21@nsut.ac.in>
2023-07-20 16:52:27 +0530

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
 var preload = document.getElementById("loader");
 setTimeout(function load() {
   preload.style.opacity = "0";
   preload.style.transform = "translate3d(-1520px, 0, 0)";
-},2000);
+}, 2000);
 
 var audioElement = false;
 $(".sound-div a").click(function () {
@@ -24,22 +23,30 @@ $(".sound-div a").click(function () {
 });
 
 const Texts = [
+  "C++",
+  " C",
+  " Git",
+  " Javascript",
+  " Express JS",
+  " React JS",
+  " jQuery",
+  " MongoDB",
+  " EJS",
+  " Pandas",
+  " REST ",
+  " MySQL",
+  " EmailJs",
+  " LeafFleatJs",
   "HTML",
   "CSS",
-  "ES5/ES6",
-  "TypeScript",
   "REST",
   "JSON",
   "GSAP",
-  "Data Science",
-  "Wordpress",
-  "PHP",
   "Python",
   "Node JS ",
   "Bootstrap",
-  "SASS",
+  "SCSS",
   "jQuery",
-  "BEM",
 ];
 
 var tagCloud = TagCloud(".sphereContainer", Texts, {
@@ -76,7 +83,7 @@ document.querySelector(".sphereContainer").style.color = "#08fdd8";
 //   repeatDelay: 2
 // });
 // gsap.from(".svg-logo", {duration:1,});
-gsap.to(".animated-img", { opacity: 1, delay: 2.2,duration:3 });
+gsap.to(".animated-img", { opacity: 1, delay: 2.2, duration: 3 });
 gsap.to(".svg-logo", { opacity: 0, delay: 2.2, duration: 15 });
 
 // for $i from 1 through 50({
@@ -84,3 +91,68 @@ gsap.to(".svg-logo", { opacity: 0, delay: 2.2, duration: 15 });
 //       animation-delay: #{$i /10}s;
 //   }
 // });
+
+function SendMail() {
+  const nameInput = document.querySelector(".name");
+  const emailInput = document.querySelector(".email");
+  const subjectInput = document.querySelector(".subject");
+  const textareaInput = document.querySelector(".textarea");
+
+  const contactForm = document.querySelector("#contact-form");
+  let email = document.querySelector(".email").value;
+  let textarea = document.querySelector(".textarea").value;
+
+  if (!email && !textarea) {
+    alert("Please fill in the required inputs");
+  } else if (!email && textarea) {
+    alert("Oops Email can't be empty");
+  } else if (!textarea && email) {
+    alert("Please provide a message");
+  } else if (email && textarea) {
+    var p = {
+      from_name: nameInput.value,
+      from_email: email,
+      message: textarea,
+    };
+    emailjs.send("service_k94trc3", "template_blmp8rw", p).then((res) => {
+      alert("Email successfully sent!");
+    });
+    //
+    textareaInput.value = "";
+    emailInput.value = "";
+    nameInput.value = "";
+    subjectInput.value = "";
+  }
+}
+document.querySelector(".bt").addEventListener ("click", SendMail);
+const setError = (input) => {
+  if (input.classList.contains("success")) {
+    input.classList.remove("success");
+  } else {
+    input.classList.add("error");
+  }
+};
+const setSuccess = (input) => {
+  if (input.classList.contains("error")) {
+    input.classList.remove("error");
+  } else {
+    input.classList.add("success");
+  }
+};
+
+const messageDiv = document.querySelector(".message");
+const showMessage = (message, updateColor) => {
+  const divContent = document.createElement("div");
+  divContent.textContent = message;
+  divContent.classList.add("message-content");
+  divContent.style.backgroundColor = updateColor;
+  messageDiv.prepend(divContent);
+
+  messageDiv.style.transform = `translate(${(0, 0)}%)`;
+  setTimeout(() => {
+    divContent.classList.add("hide");
+    divContent.addEventListener("transitionend", () => {
+      divContent.remove();
+    });
+  }, 5000);
+};
